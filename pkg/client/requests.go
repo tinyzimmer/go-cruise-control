f97fc90e7e28a63ac87986eafe903b830985f136
tinyzimmer <38474291+tinyzimmer@users.noreply.github.com>
2019-12-07 07:11:26 -0800

    use constants for endpoints

--- pkg/client/requests.go
+++ pkg/client/requests.go
@@ -1,7 +1,27 @@
 package client
 
 import (
+	"net/http"
+
 	"github.com/tinyzimmer/go-cruise-control/pkg/types"
+)
+
+const (
+	endpointKafkaClusterState = "kafka_cluster_state"
+	endpointLoad              = "load"
+	endpointPartitionLoad     = "partition_load"
+	endpointState             = "state"
+	endpointUserTasks         = "user_tasks"
+	endpointProposals         = "proposals"
+
+	endpointRebalance             = "rebalance"
+	endpointAddBroker             = "add_broker"
+	endpointRemoveBroker          = "remove_broker"
+	endpointDemoteBroker          = "demote_broker"
+	endpointFixOfflineReplicas    = "fix_offline_replicas"
+	endpointStopProposalExecution = "stop_proposal_execution"
+	endpointPauseSampling         = "pause_sampling"
+	endpointResumeSampling        = "resume_sampling"
 )
 
 // GET Methods
@@ -9,40 +29,40 @@ import (
 // GetKafkaClusterState queries the health of partitions on the cluster.
 func (c *Client) GetKafkaClusterState(opts *types.GetKafkaClusterStateRequest) (*types.GetKafkaClusterStateResponse, error) {
 	res := &types.GetKafkaClusterStateResponse{}
-	return res, c.basicRequest("GET", "kafka_cluster_state", opts, res)
+	return res, c.basicRequest(http.MethodGet, endpointKafkaClusterState, opts, res)
 }
 
 // GetLoad queries the kafka cluster load. Monitor state must be RUNNING for this
 // method to succeed.
 func (c *Client) GetLoad(opts *types.GetLoadRequest) (*types.GetLoadResponse, error) {
 	res := &types.GetLoadResponse{}
-	return res, c.basicRequest("GET", "load", opts, res)
+	return res, c.basicRequest(http.MethodGet, endpointLoad, opts, res)
 }
 
 // GetPartitionLoad queries the partition load sorted by the resource specified
 // in the request parameters.
 func (c *Client) GetPartitionLoad(opts *types.GetPartitionLoadRequest) (*types.GetPartitionLoadResponse, error) {
 	res := &types.GetPartitionLoadResponse{}
-	return res, c.basicRequest("GET", "partition_load", opts, res)
+	return res, c.basicRequest(http.MethodGet, endpointPartitionLoad, opts, res)
 }
 
 // GetState queries the state of Cruise Control.
 func (c *Client) GetState(opts *types.GetStateRequest) (*types.GetStateResponse, error) {
 	res := &types.GetStateResponse{}
-	return res, c.basicRequest("GET", "state", opts, res)
+	return res, c.basicRequest(http.MethodGet, endpointState, opts, res)
 }
 
 // GetUserTasks returns a list of all active and completed tasks in cruise control.
 func (c *Client) GetUserTasks(opts *types.GetUserTasksRequest) (*types.GetUserTasksResponse, error) {
 	res := &types.GetUserTasksResponse{}
-	return res, c.basicRequest("GET", "user_tasks", opts, res)
+	return res, c.basicRequest(http.MethodGet, endpointUserTasks, opts, res)
 }
 
 // GetProposals returns the optimization proposals generated based on workload
 // models for the provided time range.
 func (c *Client) GetProposals(opts *types.GetProposalsRequest) (*types.ProposalsResponse, error) {
 	res := &types.ProposalsResponse{}
-	return res, c.basicRequest("GET", "proposals", opts, res)
+	return res, c.basicRequest(http.MethodGet, endpointProposals, opts, res)
 }
 
 // POST Methods
@@ -50,49 +70,49 @@ func (c *Client) GetProposals(opts *types.GetProposalsRequest) (*types.Proposals
 // Rebalance requests cruise control to rebalance the kafka cluster.
 func (c *Client) Rebalance(opts *types.RebalanceRequest) (*types.ProposalsResponse, error) {
 	res := &types.ProposalsResponse{}
-	return res, c.basicRequest("POST", "rebalance", opts, res)
+	return res, c.basicRequest(http.MethodPost, endpointRebalance, opts, res)
 }
 
 // AddBrokers will add the given brokers to the kafka cluster.
 func (c *Client) AddBrokers(opts *types.AddBrokersRequest) (*types.ProposalsResponse, error) {
 	res := &types.ProposalsResponse{}
-	return res, c.basicRequest("POST", "add_broker", opts, res)
+	return res, c.basicRequest(http.MethodPost, endpointAddBroker, opts, res)
 }
 
 // RemoveBrokers will remove the given brokers from the kafka cluster.
 func (c *Client) RemoveBrokers(opts *types.RemoveBrokersRequest) (*types.ProposalsResponse, error) {
 	res := &types.ProposalsResponse{}
-	return res, c.basicRequest("POST", "remove_broker", opts, res)
+	return res, c.basicRequest(http.MethodPost, endpointRemoveBroker, opts, res)
 }
 
 // DemoteBrokers will remove any partition leadership from the given brokers.
 func (c *Client) DemoteBrokers(opts *types.DemoteBrokersRequest) (*types.ProposalsResponse, error) {
 	res := &types.ProposalsResponse{}
-	return res, c.basicRequest("POST", "demote_broker", opts, res)
+	return res, c.basicRequest(http.MethodPost, endpointDemoteBroker, opts, res)
 }
 
 // FixOfflineReplicas will move all offline replicas from dead brokers/disks.
 // This method requires Kafka > 2.x.
 func (c *Client) FixOfflineReplicas(opts *types.FixOfflineReplicasRequest) (*types.ProposalsResponse, error) {
 	res := &types.ProposalsResponse{}
-	return res, c.basicRequest("POST", "fix_offline_replicas", opts, res)
+	return res, c.basicRequest(http.MethodPost, endpointFixOfflineReplicas, opts, res)
 }
 
 // StopProposalExecution will tell cruise control to stop an ongoing POST
 // operation.
 func (c *Client) StopProposalExecution() (*types.GenericResponse, error) {
 	res := &types.GenericResponse{}
-	return res, c.basicRequest("POST", "stop_proposal_execution", nil, res)
+	return res, c.basicRequest(http.MethodPost, endpointStopProposalExecution, nil, res)
 }
 
 // PauseSampling will pause an ongoing metrics sampling process
 func (c *Client) PauseSampling(opts *types.TriggerSamplingRequest) (*types.GenericResponse, error) {
 	res := &types.GenericResponse{}
-	return res, c.basicRequest("POST", "pause_sampling", opts, res)
+	return res, c.basicRequest(http.MethodPost, endpointPauseSampling, opts, res)
 }
 
 // ResumeSampling will resume a paused metrics sampling process.
 func (c *Client) ResumeSampling(opts *types.TriggerSamplingRequest) (*types.GenericResponse, error) {
 	res := &types.GenericResponse{}
-	return res, c.basicRequest("POST", "resume_sampling", opts, res)
+	return res, c.basicRequest(http.MethodPost, endpointResumeSampling, opts, res)
 }
